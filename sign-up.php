d384a9b3df31c86f19d5a05b589a4ae2d4cf4c28
Rivaldz <rivalzsetyo@gmail.com>
2019-12-12 22:34:49 +0700

    end (maybe)

--- sign-up.php
+++ sign-up.php
@@ -6,8 +6,50 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KotakHitam-SignUp</title>
     <link rel="stylesheet" type="text/css" href="css/sign-up.css">
+     
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body id="body">
+<body id="boy">
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">KotakHitam.Com</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="tester.html" class="nav-link">Tester</a></li>
+	          <li class="nav-item"><a href="sign-up.php" class="nav-link">Register</a></li>
+	          <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+	          <li class="nav-item cta mr-md-1"><a href="new-post.html" class="nav-link">Post a Job Testing</a></li>
+	          <li class="nav-item cta cta-colored"><a href="job-post.html" class="nav-link">Want a Job Testing</a></li>
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
 <div id="login-box" >
 <form action="login.php" method="post">
     <div class="left">
